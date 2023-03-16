31b78fc9c728b0245b848de6dc9c0f41088aa881
Alston Liu <alstonliu730@gmail.com>
2023-03-15 22:20:04 -0400

    "Implemented brighten and darken function for Color."

--- src/model/Color.java
+++ src/model/Color.java
@@ -25,5 +25,27 @@ public class Color {
     this.blue = b;
   }
 
+  /**
+   * Brightens the color by a factor of luma.
+   * - The luma formula is: luma = 0.2126r + 0.7512g + 0.0722b
+   * - Then modify the values of this color
+   */
+  public void brighten() {
+    float luma = (float) (0.2126 * this.red + 0.7512 * this.green + 0.0722 * this.blue);
+    this.red = Math.round(this.red + luma) <= 255 ? Math.round(this.red + luma) : 255;
+    this.green = Math.round(this.green + luma) <= 255 ? Math.round(this.green + luma) : 255;
+    this.blue = Math.round(this.blue + luma) <= 255 ? Math.round(this.blue + luma) : 255;
+  }
 
+  /**
+   * Darkens the color by a factor of luma.
+   * - The luma formula is: luma = 0.2126r + 0.7512g + 0.0722b
+   * - Then modify the values of this color
+   */
+  public void darken() {
+    float luma = (float) (0.2126 * this.red + 0.7512 * this.green + 0.0722 * this.blue);
+    this.red = Math.round(this.red - luma) >= 0 ? Math.round(this.red - luma) : 0;
+    this.green = Math.round(this.green - luma) >= 0 ? Math.round(this.green - luma) : 0;
+    this.blue = Math.round(this.blue - luma) >= 0 ? Math.round(this.blue - luma) : 0;
+  }
 }
