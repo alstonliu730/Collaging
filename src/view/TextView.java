136542f5f02061c78647ea25fa75e45c17549edb
root <root@Alston-PC>
2023-03-17 17:42:47 -0400

    Implemented functions of the simple view

--- src/view/TextView.java
+++ src/view/TextView.java
@@ -1,2 +1,33 @@
-package view;public interface TextView {
+package view;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public interface TextView {
+  /**
+   * Produces a textual view of the pixels in the project.
+   * The first 3 rows of output will be the project information.
+   * Then the view will output the Layers and the Layer Content.
+   *
+   * @return - representation of the current Project state
+   */
+  public String toString();
+
+  /**
+   * Renders the given message into the data output of the implementation.
+   *
+   * @param message - the message to be printed
+   * @throws IOException - if the transmission of the message to the data output fails
+   */
+  public void renderMessage(String message) throws IOException;
+
+  /**
+   * Renders the project into the data output of the implementation
+   *
+   * @throws IOException - if the transmission of the project to the data output fails
+   */
+  public void renderProject() throws IOException;
+
 }
