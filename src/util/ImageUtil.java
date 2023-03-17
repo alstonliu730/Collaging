856ea2da840bd30990bf1197b9010e195ab010be
root <root@Alston-PC>
2023-03-17 17:44:21 -0400

    Output of readPPM is a list of pixels

--- src/util/ImageUtil.java
+++ src/util/ImageUtil.java
@@ -1,6 +1,13 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
+
+import model.Pixel;
+import model.Posn;
 
 /**
  * This class contains utility methods to read a PPM image from file and simply print its contents. Feel free to change this method 
@@ -13,16 +20,17 @@ public class ImageUtil {
    *
    * @param filename the path of the file. 
    */
-  public static void readPPM(String filename) {
+  public static List<Pixel> readPPM(String filename) throws FileNotFoundException {
     Scanner sc;
-    
+    List<Pixel> pixelList = new ArrayList<Pixel>();
+
     try {
         sc = new Scanner(new FileInputStream(filename));
     }
     catch (FileNotFoundException e) {
-        System.out.println("File "+filename+ " not found!");
-        return;
+        throw new FileNotFoundException("File "+filename+ " not found!");
     }
+
     StringBuilder builder = new StringBuilder();
     //read the file line by line, and populate a string. This will throw away any comment lines
     while (sc.hasNextLine()) {
@@ -42,34 +50,23 @@ public class ImageUtil {
         System.out.println("Invalid PPM file: plain RAW file should begin with P3");
     }
     int width = sc.nextInt();
-    System.out.println("Width of image: "+width);
+    System.out.println("Width of image: " + width);
     int height = sc.nextInt();
-    System.out.println("Height of image: "+height);
+    System.out.println("Height of image: " + height);
     int maxValue = sc.nextInt();
-    System.out.println("Maximum value of a color in this file (usually 255): "+maxValue);
-    
-    for (int i=0;i<height;i++) {
-        for (int j=0;j<width;j++) {
+    System.out.println("Maximum value of a color in this file (usually 255): " + maxValue);
+
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
             int r = sc.nextInt();
             int g = sc.nextInt();
             int b = sc.nextInt();
-            System.out.println("Color of pixel ("+ j +","+ i +"): "+ r +","+ g +"," + b);
+            pixelList.add(new Pixel(r,g,b,maxValue,new Posn(i,j)));
         }
     }
-  }
 
-  //demo main
-  public static void main(String []args) {
-      String filename;
-      
-      if (args.length > 0) {
-          filename = args[0];
-      }
-      else {
-          filename = "sample.ppm";
-      }
-      
-      ImageUtil.readPPM(filename);
+    return pixelList;
   }
 }
 
